18039c915181b0f5b23bca96d3c959aa5b31ead4
Knosero <rafalpilecki.szczecin@gmail.com>
2021-01-07 02:43:17 +0100

    fix heroku

--- script/scraping.js
+++ script/scraping.js
@@ -103,7 +103,12 @@ class Scraping {
         }
 
         //WYSZUKANIE STRONY
-        const browser = await puppeteer.launch();
+        const browser = await puppeteer.launch({
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+            ],
+        });
         const page = await browser.newPage();
         await returnPage();
 
